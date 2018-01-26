bbfd913756ebf70a9b6af3756bc34a77b3eff691
hp ios <hpios@hps-Mac.local>
2018-01-26 04:29:41 -0800

    Initial Commit

---
+++ AlmofireWhetherRESTAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlmofireWhetherRESTAPI
+//
+//  Created by hp ios on 1/26/18.
+//  Copyright © 2018 andiosdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
